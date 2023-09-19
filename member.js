923489a9dddfb77ebc45645ce1417aca3dec3996
mafazi08 <51124712+mafazi08@users.noreply.github.com>
2023-09-19 02:15:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMembers() {
+    var skills = document.getElementById('skills');
+    var members = document.getElementById('members');
+    var membersList = document.getElementById('membersList');
+    var skillsList = document.getElementById('skillsList');
+    members.style.display = 'block';
+    skills.style.display = 'none';
+    membersList.style.display = 'block';
+    skillsList.style.display = 'none';
+}
